21d316481f77a39f89137e29cb4aa013ce0b7d10
Mike <barbeque@users.noreply.github.com>
2025-06-30 18:26:18 -0600

    Put page numbers at bottom

--- pdf-append.sh
+++ pdf-append.sh
@@ -1,2 +1,2 @@
 # append PDFs with page numbers starting at 1
-convert -pointsize 12 -annotate 0x0+10+20 "Page %[fx:t+1] of %n" $1 $2
+convert -pointsize 12 -gravity south -annotate 0x0+0+0 "Page %[fx:t+1] of %n" $1 $2
