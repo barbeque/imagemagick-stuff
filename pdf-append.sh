ef08c632ae06fbb3775e9bf11b6f635c47eba7aa
Mike <barbeque@users.noreply.github.com>
2025-07-03 07:47:54 -0400

    Update pdf-append.sh

--- pdf-append.sh
+++ pdf-append.sh
@@ -2,3 +2,4 @@
 convert -pointsize 12 -gravity south -annotate 0x0+0+0 "Page %[fx:t+1] of %n" $1 $2
 
 # on windows, use magick and reorder the arguments to magick {input files} {parameters} {output file} and also make sure you installed ghostscript for windows otherwise it will pitch a fit about gswin64c or something similar
+# TODO: fix conditionals
